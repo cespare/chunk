149d14ca9bbba0097f3c9013f5cb219529896fb7
Caleb Spare <cespare@gmail.com>
2015-08-06 19:21:14 -0700

    Accept more representations of numbers

--- chunk.go
+++ chunk.go
@@ -4,19 +4,28 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"math"
 	"os"
 	"strconv"
+
+	"github.com/dustin/go-humanize"
 )
 
 func main() {
-	end := flag.Int64("end", 0, "Ending offset")
-	lenf := flag.Int64("len", 0, "Length of chunk")
+	var (
+		off  Int64
+		end  Int64
+		lenf Int64
+	)
+	flag.Var(&end, "end", "Ending offset")
+	flag.Var(&lenf, "len", "Length of chunk")
 	flag.Usage = func() {
 		fmt.Printf(`Usage: %s [OPTIONS] FILENAME OFFSET
 where OPTIONS are:
 `, os.Args[0])
 		flag.PrintDefaults()
-		fmt.Println("(Exactly one of -end, -len must be given.)")
+		fmt.Println(`Exactly one of -end, -len must be given.
+Numbers may be written as 1000, 1e3, or 1kB.`)
 	}
 	flag.Parse()
 
@@ -30,33 +39,62 @@ where OPTIONS are:
 		os.Exit(1)
 	}
 	defer f.Close()
-	off, err := strconv.ParseInt(flag.Arg(1), 10, 64)
-	if err != nil {
-		fmt.Printf("Cannot parse offset: %s\n", err)
-		os.Exit(1)
+	if err := off.Set(flag.Arg(1)); err != nil {
+		Fatalf("Error with offset: %s\n", err)
 	}
 
 	switch {
-	case *end == 0 && *lenf == 0:
+	case end == 0 && lenf == 0:
 		Fatalln("One of -end, -len must be given.")
-	case *end != 0 && *lenf != 0:
+	case end != 0 && lenf != 0:
 		Fatalln("Only one of -end, -len may be given.")
-	case *end < 0:
+	case end < 0:
 		Fatalln("-end cannot be negative.")
-	case *end < 0:
+	case end < 0:
 		Fatalln("-end cannot be negative.")
-	case *end > 0 && *end <= off:
+	case end > 0 && end <= off:
 		Fatalln("-end must be greater than the offset.")
 	}
 
-	n := *lenf
+	n := lenf
 	if n == 0 {
-		n = *end - off
+		n = end - off
 	}
-	if _, err := io.Copy(os.Stdout, io.NewSectionReader(f, off, n)); err != nil {
+	sr := io.NewSectionReader(f, int64(off), int64(n))
+	if _, err := io.Copy(os.Stdout, sr); err != nil {
 		fmt.Printf("Error while reading chunk: %s\n", err)
 		os.Exit(1)
 	}
+}
+
+type Int64 int64
+
+func (n *Int64) String() string { return fmt.Sprintf("%d", *n) }
+func (n *Int64) Set(s string) error {
+	nn, err := strconv.ParseInt(s, 10, 64)
+	if err == nil {
+		*n = Int64(nn)
+		return nil
+	}
+	f, err := strconv.ParseFloat(s, 64)
+	if err == nil {
+		if f > float64(math.MaxInt64) {
+			return fmt.Errorf("float value too large for float64: %g", f)
+		}
+		*n = Int64(f)
+		return nil
+	}
+	u, err := humanize.ParseBytes(s)
+	if err == nil {
+		*n = Int64(u)
+		return nil
+	}
+	return fmt.Errorf("cannot parse %q", s)
+}
+
+func Fatalf(format string, args ...interface{}) {
+	fmt.Printf(format, args...)
+	os.Exit(1)
 }
 
 func Fatalln(args ...interface{}) {
